8d42d930a0291db99c4304ff47d7586dd4840c81
changgun <foryou0810@gmail.com>
2015-06-10 01:57:20 +0900

    Secure data app.
    BT manager, key exchange, aes-cbc, key generation

--- android-client/gen/com/redcarrottt/remotekeyclient/R.java
+++ android-client/gen/com/redcarrottt/remotekeyclient/R.java
@@ -25,16 +25,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int connectedDeviceTextView_mainActivity=0x7f080005;
-        public static final int deviceListView=0x7f080004;
-        public static final int deviceListView_deviceNameTextView=0x7f080007;
-        public static final int deviceListView_macAddressTextView=0x7f080008;
-        public static final int disconnectDeviceButton_mainActivity=0x7f080002;
-        public static final int homeButton_mainActivity=0x7f080006;
-        public static final int statusTextView_mainActivity=0x7f080000;
-        public static final int turnOnOffBluetoothPButton_mainActivity=0x7f080003;
-        public static final int updateDeviceListButton_mainActivity=0x7f080001;
+        public static final int action_settings=0x7f08000a;
+        public static final int connectedDeviceTextView_mainActivity=0x7f080006;
+        public static final int deviceListView=0x7f080002;
+        public static final int deviceListView_deviceNameTextView=0x7f080008;
+        public static final int deviceListView_macAddressTextView=0x7f080009;
+        public static final int disconnectDeviceButton_mainActivity=0x7f080001;
+        public static final int editText=0x7f080003;
+        public static final int homeButton_mainActivity=0x7f080004;
+        public static final int menuButton_mainActivity=0x7f080005;
+        public static final int statusTextView_mainActivity=0x7f080007;
+        public static final int updateDeviceListButton_mainActivity=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- android-client/src/com/redcarrottt/remotekeyclient/MainActivity.java
+++ android-client/src/com/redcarrottt/remotekeyclient/MainActivity.java
@@ -22,6 +22,8 @@ import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,8 +32,19 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+
+
+
+
+
+import android.widget.Toast;
+
+import com.chilkatsoft.*;
+
 
 public class MainActivity extends Activity implements OnClickListener {
 	private BluetoothAdapter mBluetoothAdapter = null;
@@ -39,13 +52,13 @@ public class MainActivity extends Activity implements OnClickListener {
 	private ListView mDeviceListView;
 	private TextView mStatusTextView;
 	private TextView mConnectedDeviceTextView;
+	
+	private EditText mEditText;
 
 	private Button mUpdateDeviceListButton;
 	private Button mDisconnectDeviceButton;
-	private Button mTurnOnOffBluetoothButton;
 	private Button mMenuButton;
 	private Button mHomeButton;
-	private Button mBackButton;
 
 	private BluetoothConnector mBluetoothConnector = null;
 	private BluetoothInteractor mBluetoothInteractor = null;
@@ -53,18 +66,47 @@ public class MainActivity extends Activity implements OnClickListener {
 	private BluetoothConnectionChecker mBluetoothConnectionChecker;
 
 	private boolean mIsDeviceListEnabled = true;
+	
+	private CkDh dhBob;
+    private CkDh dhAlice;
+    private String eBob;
+    private String eAlice;
+    private String sendKey;
 
-	// Message strings
-	private final String MSG_MENU_BUTTON = "menu";
-	private final String MSG_HOME_BUTTON = "home";
-	private final String MSG_BACK_BUTTON = "back";
+	
+	private static final String TAG = "Chilkat";
+	
+	
+	 static {
+	      // Important: Make sure the name passed to loadLibrary matches the shared library
+	      // found in your project's libs/armeabi directory.
+	      //  for "libchilkat.so", pass "chilkat" to loadLibrary
+	      //  for "libchilkatemail.so", pass "chilkatemail" to loadLibrary
+	      //  etc.
+	      // 
+	      System.loadLibrary("chilkat");
 
+	      // Note: If the incorrect library name is passed to System.loadLibrary,
+	      // then you will see the following error message at application startup:
+	      //"The application <your-application-name> has stopped unexpectedly. Please try again."
+	  }
+	 
+	
 	// Application Lifecycle
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		dhBob = new CkDh();
+	    dhAlice = new CkDh();
 
+		
+		
+		keySHARE();
+		
+		
+		
 		// Ensure bluetooth connection or quit app.
 		boolean succeed = ensureBluetoothConnection();
 		if (succeed == false) {
@@ -87,16 +129,15 @@ public class MainActivity extends Activity implements OnClickListener {
 		// Set Buttons
 		this.mUpdateDeviceListButton = (Button) findViewById(R.id.updateDeviceListButton_mainActivity);
 		this.mDisconnectDeviceButton = (Button) findViewById(R.id.disconnectDeviceButton_mainActivity);
-		this.mTurnOnOffBluetoothButton = (Button) findViewById(R.id.turnOnOffBluetoothPButton_mainActivity);
 		this.mMenuButton = (Button) findViewById(R.id.menuButton_mainActivity);
 		this.mHomeButton = (Button) findViewById(R.id.homeButton_mainActivity);
-		this.mBackButton = (Button) findViewById(R.id.backButton_mainActivity);
 		this.mUpdateDeviceListButton.setOnClickListener(this);
 		this.mDisconnectDeviceButton.setOnClickListener(this);
-		this.mTurnOnOffBluetoothButton.setOnClickListener(this);
 		this.mMenuButton.setOnClickListener(this);
 		this.mHomeButton.setOnClickListener(this);
-		this.mBackButton.setOnClickListener(this);
+		
+		// Edit Text
+		this.mEditText = (EditText) findViewById(R.id.editText);
 
 		this.updateTurnOnOffBluetoothButton();
 
@@ -109,7 +150,118 @@ public class MainActivity extends Activity implements OnClickListener {
 		// In default, update device list view
 		this.updateDeviceList();
 	}
+	
+	
+	
+	public void keyShare_send() {
+		
+	    boolean success;
 
+	    //  Unlock the component once at program startup...
+	    success = dhBob.UnlockComponent("Anything for 30-day trial");
+	    if (success != true) {
+	        Log.i(TAG, dhBob.lastErrorText());
+	        return;
+	    }
+
+	    dhBob.UseKnownPrime(2);
+	    String p;
+	    int g;
+	    //  Bob will now send P and G to Alice.
+	    
+	    // 1. P, 2. G 보내고,
+	    p = dhBob.p();
+	    this.pressButton(p);
+	    
+	    g = dhBob.get_G();
+	    String gtemp = Integer.toString(g);
+	    this.pressButton(gtemp);
+	    
+	    success = dhAlice.SetPG(p,g);
+	    
+	    if (success != true) {
+	        Log.i(TAG, "P is not a safe prime");
+	        return;
+	    }
+
+	    String eBob;
+	    eBob = dhBob.createE(256);
+	    
+	    // 3. eBob 보내고
+	    this.pressButton(eBob);   
+	}
+	
+	public void keyShare_receive(String rev) {
+		
+		boolean success;
+		
+		// 4. eAlice 받고 KBob을 구한다.
+
+	    //  Alice does the same:
+//	    String eAlice;
+//	    eAlice = dhAlice.createE(256);
+		
+		eAlice = rev;
+
+	    //  The "E" values are sent over the insecure channel.
+	    //  Bob sends his "E" to Alice, and Alice sends her "E" to Bob.
+
+	    //  Each side computes the shared secret by calling FindK.
+	    //  "K" is the shared-secret.
+
+	    String kBob;
+	    String kAlice;
+
+	    //  Bob computes the shared secret from Alice's "E":
+	    kBob = dhBob.findK(eAlice);
+
+	    //  Alice computes the shared secret from Bob's "E":
+	    kAlice = dhAlice.findK(eBob);
+
+	    CkCrypt2 crypt = new CkCrypt2();
+	    success = crypt.UnlockComponent("Anything for 30-day trial.");
+	    if (success != true) {
+	        Log.i(TAG, crypt.lastErrorText());
+	        return;
+	    }
+
+	    crypt.put_EncodingMode("hex");
+	    crypt.put_HashAlgorithm("md5");
+
+	    String sessionKey;
+	    sessionKey = crypt.hashStringENC(kBob);
+
+	    Log.i(TAG, "128-bit Session Key:");
+	    Log.i(TAG, sessionKey);
+
+	    //  Encrypt something...
+	    crypt.put_CryptAlgorithm("aes");
+	    crypt.put_KeyLength(128);
+	    crypt.put_CipherMode("cbc");
+
+	    //  Use an IV that is the MD5 hash of the session key...
+	    String iv;
+	    iv = crypt.hashStringENC(sessionKey);
+
+	    crypt.SetEncodedKey(sessionKey,"hex");
+	    crypt.SetEncodedIV(iv,"hex");
+
+	    //  Encrypt some text:
+	    String cipherText64;
+
+	    crypt.put_EncodingMode("base64");
+	    cipherText64 = crypt.encryptStringENC(sendKey);
+
+	    String plainText;
+	    plainText = crypt.decryptStringENC(cipherText64);
+	    
+	    // Last Send
+	    pressButton(cipherText64);
+	    Toast.makeText(MainActivity.this, cipherText64, Toast.LENGTH_LONG).show();
+	}
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -174,14 +326,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		this.mUpdateDeviceListButton.setEnabled(false);
 	}
 
-	private void _setAsTurnOnDeviceButton() {
-		this.mTurnOnOffBluetoothButton.setText("BT ON");
-	}
-
-	private void _setAsTurnOffDeviceButton() {
-		this.mTurnOnOffBluetoothButton.setText("BT OFF");
-	}
-
 	// Functionalities of this activity
 	private boolean ensureBluetoothConnection() {
 		this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -198,15 +342,6 @@ public class MainActivity extends Activity implements OnClickListener {
 			startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
 		}
 		return true;
-	}
-
-	private void turnOnOrOffBluetooth() {
-		if (mBluetoothAdapter.getState() == BluetoothAdapter.STATE_TURNING_ON
-				|| mBluetoothAdapter.getState() == BluetoothAdapter.STATE_ON) {
-			this.mBluetoothAdapter.disable();
-		} else {
-			this.mBluetoothAdapter.enable();
-		}
 	}
 
 	private void tryToConnectDevice(BluetoothDevice device) {
@@ -245,7 +380,6 @@ public class MainActivity extends Activity implements OnClickListener {
 
 			_updateStatusTextView("Bluetooth is turned on. Updating device list...");
 
-			_setAsTurnOffDeviceButton();
 			_enableUpdateDeviceListButton();
 			_disableDisconnectDeviceButton();
 
@@ -263,13 +397,14 @@ public class MainActivity extends Activity implements OnClickListener {
 			updateDeviceList();
 			_updateStatusTextView("Bluetooth is turned off");
 
-			_setAsTurnOnDeviceButton();
 			_disableUpdateDeviceListButton();
 
 			_disableDeviceListView();
 		}
 	}
 
+	//---------------------------------------------------------------------------------
+	// send event: Test key
 	private void pressButton(String buttonMessage) {
 		if (this.mBluetoothInteractor == null) {
 			DialogUtility.showDialogSimple(this,
@@ -280,31 +415,37 @@ public class MainActivity extends Activity implements OnClickListener {
 		BluetoothMessage msg = new BluetoothMessage(buttonMessage);
 		boolean ret = this.mBluetoothInteractor.sendMessage(msg);
 		if (ret == true) {
-			this._updateStatusTextView("Sent an button event (" + buttonMessage + ") to the target");
+			this._updateStatusTextView("Message: " + buttonMessage);
 		} else {
-			this._updateStatusTextView("Failed to send the button event (" + buttonMessage + ") to the target");
+			this._updateStatusTextView("Failed to send the event");
 		}
 	}
 
+	//----------------------------------------------------------------------
+	// Test Key Send
 	private void pressMenuButton() {
-		this.pressButton(MSG_MENU_BUTTON);
+		// original key
+		Editable etStr = mEditText.getText();
+		this.pressButton(etStr.toString());
 	}
-
+	
+	//----------------------------------------------------------------------
+	// Security Key Send
 	private void pressHomeButton() {
-		this.pressButton(MSG_HOME_BUTTON);
-	}
-
-	private void pressBackButton() {
-		this.pressButton(MSG_BACK_BUTTON);
+		Toast.makeText(MainActivity.this, "S-Key generate", Toast.LENGTH_LONG).show();
+		keyShare_send();
+		
+		// original key
+		Editable etTxt = mEditText.getText();
+		sendKey = etTxt.toString();
+//		this.pressButton(etStr.toString());
 	}
 
 	// OnClickListener of buttons
 	@Override
 	public void onClick(View view) {
-		if (view == this.mTurnOnOffBluetoothButton) {
-			// If turn on off Bluetooth is on click
-			this.turnOnOrOffBluetooth();
-		} else if (view == this.mUpdateDeviceListButton) {
+
+		if (view == this.mUpdateDeviceListButton) {
 			// If update device list button is on click
 			this.updateDeviceList();
 		}
@@ -317,9 +458,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		} else if (view == this.mHomeButton) {
 			// If home button is on click
 			this.pressHomeButton();
-		} else if (view == this.mBackButton) {
-			// If back button is on click
-			this.pressBackButton();
 		}
 	}
 
@@ -386,8 +524,11 @@ public class MainActivity extends Activity implements OnClickListener {
 		Runnable runnableOnUIThread = new Runnable() {
 			@Override
 			public void run() {
+				Toast.makeText(MainActivity.this, "Receive Data", Toast.LENGTH_LONG).show();
 				_updateStatusTextView("The button is pressed!: "
 						+ fMessage.toString());
+				
+				keyShare_receive(fMessage.toString());
 			}
 		};
 		this.runOnUiThread(runnableOnUIThread);
@@ -654,5 +795,175 @@ public class MainActivity extends Activity implements OnClickListener {
 					.toCharArray()));
 			return byteBuffer.array();
 		}
+	}
+	
+	public void keySHARE() {
+		 
+	    //  Create two separate instances of the DH object.
+	    CkDh dhBob = new CkDh();
+	    CkDh dhAlice = new CkDh();
+
+	    boolean success;
+
+	    //  Unlock the component once at program startup...
+	    success = dhBob.UnlockComponent("Anything for 30-day trial");
+	    if (success != true) {
+	        Log.i(TAG, dhBob.lastErrorText());
+	        return;
+	    }
+
+	    //  The DH algorithm begins with a large prime, P, and a generator, G.
+	    //  These don't have to be secret, and they may be transmitted over an insecure channel.
+	    //  The generator is a small integer and typically has the value 2 or 5.
+
+	    //  The Chilkat DH component provides the ability to use known
+	    //  "safe" primes, as well as a method to generate new safe primes.
+
+	    //  This example will use a known safe prime.  Generating
+	    //  new safe primes is a time-consuming CPU intensive task
+	    //  and is normally done offline.
+
+	    //  Bob will choose to use the 2nd of our 8 pre-chosen safe primes.
+	    //  It is the Prime for the 2nd Oakley Group (RFC 2409) --
+	    //  1024-bit MODP Group.  Generator is 2.
+	    //  The prime is: 2^1024 - 2^960 - 1 + 2^64 * { [2^894 pi] + 129093 }
+	    dhBob.UseKnownPrime(2);
+	    
+	    //  The computed shared secret will be equal to the size of the prime (in bits).
+	    //  In this case the prime is 1024 bits, so the shared secret will be 128 bytes (128 * 8 = 1024).
+	    //  However, the result is returned as an SSH1-encoded bignum in hex string format.
+	    //  The SSH1-encoding prepends a 2-byte count, so the result is going  to be 2 bytes
+	    //  longer: 130 bytes.  This results in a hex string that is 260 characters long (two chars
+	    //  per byte for the hex encoding).
+
+	    String p;
+	    int g;
+	    //  Bob will now send P and G to Alice.
+	    
+	    // 1. P, 2. G 보내고,
+	    p = dhBob.p();
+	    g = dhBob.get_G();
+
+	    Log.i(TAG, "p:" + p);
+	    Log.i(TAG, "g:" + g);
+	    
+	    //  Alice calls SetPG to set P and G.  SetPG checks
+	    //  the values to make sure it's a safe prime and will
+	    //  return false if not.
+	    success = dhAlice.SetPG(p,g);
+	    if (success != true) {
+	        Log.i(TAG, "P is not a safe prime");
+	        return;
+	    }
+
+	    //  Each side begins by generating an "E"
+	    //  value.  The CreateE method has one argument: numBits.
+	    //  It should be set to twice the size of the number of bits
+	    //  in the session key.
+
+	    //  Let's say we want to generate a 128-bit session key
+	    //  for AES encryption.  The shared secret generated by the Diffie-Hellman
+	    //  algorithm will be longer, so we'll hash the result to arrive at the
+	    //  desired session key length.  However, the length of the session
+	    //  key we'll utlimately produce determines the value that should be
+	    //  passed to the CreateE method.
+
+	    //  In this case, we'll be creating a 128-bit session key, so pass 256 to CreateE.
+	    //  This setting is for security purposes only -- the value
+	    //  passed to CreateE does not change the length of the shared secret
+	    //  that is produced by Diffie-Hellman.
+	    //  Also, there is no need to pass in a value larger
+	    //  than 2 times the expected session key length.  It suffices to
+	    //  pass exactly 2 times the session key length.
+
+	    //  Bob generates a random E (which has the mathematical
+	    //  properties required for DH).
+	    eBob = dhBob.createE(256);
+	    
+	    // 3. e.Bob 보내고
+	    
+	    // 4. eAlice 받고 KBob을 구한다.
+
+	    //  Alice does the same:
+	    String eAlice;
+	    eAlice = dhAlice.createE(256);
+
+	    //  The "E" values are sent over the insecure channel.
+	    //  Bob sends his "E" to Alice, and Alice sends her "E" to Bob.
+
+	    //  Each side computes the shared secret by calling FindK.
+	    //  "K" is the shared-secret.
+
+	    String kBob;
+	    String kAlice;
+
+	    //  Bob computes the shared secret from Alice's "E":
+	    kBob = dhBob.findK(eAlice);
+
+	    //  Alice computes the shared secret from Bob's "E":
+	    kAlice = dhAlice.findK(eBob);
+
+	    
+	    Log.i(TAG, "eBob:" + eBob);
+	    Log.i(TAG, "eAlice:" + eAlice);
+	    
+	    //  Amazingly, kBob and kAlice are identical and the expected
+	    //  length (260 characters).  The strings contain the hex encoded bytes of
+	    //  our shared secret:
+	    Log.i(TAG, "Bob's shared secret:");
+	    Log.i(TAG, kBob);
+	    Log.i(TAG, "Alice's shared secret (should be equal to Bob's)");
+	    Log.i(TAG, kAlice);
+
+	    //  To arrive at a 128-bit session key for AES encryption, Bob and Alice should
+	    //  both transform the raw shared secret using a hash algorithm that produces
+	    //  the size of session key desired.   MD5 produces a 16-byte (128-bit) result, so
+	    //  this is a good choice for 128-bit AES.
+
+	    //  Here's how you would use Chilkat Crypt (a separate Chilkat component) to
+	    //  produce the session key:
+	    CkCrypt2 crypt = new CkCrypt2();
+	    success = crypt.UnlockComponent("Anything for 30-day trial.");
+	    if (success != true) {
+	        Log.i(TAG, crypt.lastErrorText());
+	        return;
+	    }
+
+	    crypt.put_EncodingMode("hex");
+	    crypt.put_HashAlgorithm("md5");
+
+	    String sessionKey;
+	    sessionKey = crypt.hashStringENC(kBob);
+
+	    Log.i(TAG, "128-bit Session Key:");
+	    Log.i(TAG, sessionKey);
+
+	    //  Encrypt something...
+	    crypt.put_CryptAlgorithm("aes");
+	    crypt.put_KeyLength(128);
+	    crypt.put_CipherMode("cbc");
+
+	    //  Use an IV that is the MD5 hash of the session key...
+	    String iv;
+	    iv = crypt.hashStringENC(sessionKey);
+
+	    //  AES uses a 16-byte IV:
+	    Log.i(TAG, "Initialization Vector:");
+	    Log.i(TAG, iv);
+
+	    crypt.SetEncodedKey(sessionKey,"hex");
+	    crypt.SetEncodedIV(iv,"hex");
+
+	    //  Encrypt some text:
+	    String cipherText64;
+
+	    crypt.put_EncodingMode("base64");
+	    cipherText64 = crypt.encryptStringENC("home");
+	    Log.i(TAG, cipherText64);
+
+	    String plainText;
+	    plainText = crypt.decryptStringENC(cipherText64);
+
+	    Log.i(TAG, plainText);
 	}
 }
